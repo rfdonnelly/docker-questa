1f97502e1b5bb3789c186046159100dd44de69f1
Rob Donnelly <rfdonnelly@gmail.com>
2021-03-12 00:22:07 -0800

    fix: support for Questa VIP 2020.4
    QVIP 2020.4 is incompatible with GCC 7.4.0. Fix by swaping GCC 7.4.0
    with GCC 5.3.0.

--- 2020.4_2/prune.sh
+++ 2020.4_2/prune.sh
@@ -4,11 +4,14 @@ QUESTA_ROOT=$1
 
 cd $QUESTA_ROOT/questasim
 
-rm -r gcc-5.3.0-linux_x86_64
+# NOTE: Questa VIP 2020.4 is incompatible with gcc-7.4.0.  Keep
+# gcc-5.3.0-linux_x86_64 for Questa VIP.
 rm -r gcc-7.4.0-linux_aarch64
+# rm -r gcc-5.3.0-linux_x86_64
+rm -r gcc-7.4.0-linux_x86_64
+rm -r gcc-4.7.4-linux_x86_64
 rm -r udbplayer
 rm -r examples
-rm -r gcc-4.7.4-linux_x86_64
 rm -r uvm-1.1c
 rm -r uvm-1.1d
 rm -r ovm-2.1.2
